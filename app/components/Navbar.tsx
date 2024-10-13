d5d2c5d979db87317c929bb26624b90e2be732c6
“SushantKumar29” <sushantkmr29@gmail.com>
2024-10-13 12:50:12 +0530

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+	const { userId } = auth();
+	return (
+		<div className='bg-cyan-900 rounded-b-xl'>
+			<ul className='flex justify-between py-4 px-6'>
+				<div>
+					<Link href='/'>
+						<li>Home</li>
+					</Link>
+				</div>
+				<div className='flex items-center'>
+					<Link href='/client'>
+						<li>Client Page</li>
+					</Link>
+				</div>
+				<div className='flex gap-6 items-center'>
+					{!userId ? (
+						<>
+							<Link href='/sign-in'>
+								<li>Login</li>
+							</Link>
+							<Link href='/sign-up'>
+								<li>Sign Up</li>
+							</Link>
+						</>
+					) : (
+						<>
+							<Link href='/profile'>
+								<li>Profile</li>
+							</Link>
+							<Link href='/sign-up'>
+								<li className='flex items-center'>
+									<UserButton />
+								</li>
+							</Link>
+						</>
+					)}
+				</div>
+			</ul>
+		</div>
+	);
+};
+
+export default Navbar;
+
+{
+	/* <SignedOut>
+						<SignInButton />
+					</SignedOut>
+					<SignedIn>
+						<UserButton />
+					</SignedIn> */
+}
---
+++ connect.ts
@@ -0,0 +1,33 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URI!;
+
+interface MongooseConnection {
+	connection: Mongoose | null;
+	promise: Promise<Mongoose> | null;
+}
+
+let cachedConnection: MongooseConnection = (global as any).mongoose;
+
+if (!cachedConnection) {
+	cachedConnection = (global as any).mongoose = {
+		connection: null,
+		promise: null,
+	};
+}
+
+export const connect = async () => {
+	if (cachedConnection.connection) {
+		return cachedConnection.connection;
+	}
+	cachedConnection.promise =
+		cachedConnection.promise ||
+		mongoose.connect(MONGODB_URL, {
+			dbName: "next-project-2024",
+			bufferCommands: false,
+			connectTimeoutMS: 30000,
+		});
+
+	cachedConnection.connection = await cachedConnection.promise;
+	return cachedConnection.connection;
+};
